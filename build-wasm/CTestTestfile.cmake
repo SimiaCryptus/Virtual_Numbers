8acdd4aae9ccaf99483007111d6c26c25c1f6af3
Andrew Charneski <acharneski@gmail.com>
2026-06-27 10:24:56 -0400

    minimum bug repro

--- build-wasm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrew/code/NAM
-# Build directory: /home/andrew/code/NAM/build-wasm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("bindings/wasm")
-subdirs("tests")
--- build.sh
+++ build.sh
@@ -2,7 +2,3 @@
 
 cmake -S . -B build
 cmake --build build --target nam_tests
-
-source ~/emsdk/emsdk_env.sh
-emcmake cmake -S . -B build-wasm -DNAM_BUILD_WASM=ON
-cmake --build build-wasm
